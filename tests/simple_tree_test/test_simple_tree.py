9a3bf477c22c676cc32b4f6fee17eabaf06ca922
henningzakrisson <henning.zakrisson@gmail.com>
2023-02-09 13:43:28 +0100

    Add missing value handling (default rule) and test, reorganize test data

--- tests/simple_tree_test/test_simple_tree.py
+++ tests/simple_tree_test/test_simple_tree.py
@@ -4,7 +4,7 @@ import pandas as pd
 class RegressionTreeTest(unittest.TestCase):
     def test_responses(self):
         from src.simple_tree.simple_tree import RegressionTree
-        df = pd.read_csv('test_data.csv',index_col = 0)
+        df = pd.read_csv('data/test_data.csv',index_col = 0)
 
         df_hat = df[['y','X_0','X_1']].copy()
         for max_depth in [1,2]:
@@ -20,7 +20,7 @@ class RegressionTreeTest(unittest.TestCase):
 
     def test_no_more_splits(self):
         from src.simple_tree.simple_tree import RegressionTree
-        df = pd.read_csv('test_data.csv',index_col = 0)
+        df = pd.read_csv('data/test_data.csv',index_col = 0)
 
         df_hat = df[['y','X_0','X_1']].copy()
         max_depth = 10
@@ -33,6 +33,21 @@ class RegressionTreeTest(unittest.TestCase):
         self.assertEqual((df_hat[f'tree_{max_depth}'].round(3)==df[f'tree_{2}'].round(3)).sum().sum(),
                          len(df))
 
+    def test_default_splitting_rule(self):
+        from src.simple_tree.simple_tree import RegressionTree
+        df = pd.read_csv('data/test_data.csv',index_col = 0)
+        df_missing = pd.read_csv('data/test_data_missing.csv',index_col = 0)
+
+        df_tot = pd.concat([df,df_missing]).reset_index()
+
+        max_depth = 2
+        tree = tree = RegressionTree(max_depth = max_depth)
+        tree.fit(df_tot[['X_0','X_1']],df_tot['y'])
+
+        df_tot['y_hat'] = tree.predict(df_tot[['X_0','X_1']])
+
+        self.assertEqual((df_tot.loc[~df_tot['y'].isna(),'y_hat'].round(2)==df_tot.loc[~df_tot['y'].isna(),'y'].round(2)).sum().sum(),
+                         sum(~df_tot['y'].isna()))
 
 if __name__ == '__main__':
     unittest.main()
