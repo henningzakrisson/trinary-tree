9f8e4d82705258cf87a09c071fb06eb0a31c0c0a
henningzakrisson <henning.zakrisson@gmail.com>
2023-02-09 12:48:50 +0100

    Add unit test for situation where no more splits are useful

--- tests/simple_tree_test/test_simple_tree.py
+++ tests/simple_tree_test/test_simple_tree.py
@@ -18,6 +18,21 @@ class RegressionTreeTest(unittest.TestCase):
         self.assertEqual((df[columns].round(3)==df_hat[columns].round(3)).sum().sum(),
                          len(columns) * len(df))
 
+    def test_no_more_splits(self):
+        from src.simple_tree.simple_tree import RegressionTree
+        df = pd.read_csv('test_data.csv',index_col = 0)
+
+        df_hat = df[['y','X_0','X_1']].copy()
+        max_depth = 10
+        tree = RegressionTree(max_depth = max_depth)
+        tree.fit(df[['X_0','X_1']],df['y'])
+        y_hat = tree.predict(df_hat[['X_0','X_1']])
+
+        df_hat.loc[:,f'tree_{max_depth}'] = y_hat.copy()
+
+        self.assertEqual((df_hat[f'tree_{max_depth}'].round(3)==df[f'tree_{2}'].round(3)).sum().sum(),
+                         len(df))
+
 
 if __name__ == '__main__':
     unittest.main()
